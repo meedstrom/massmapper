aa9b942be43e00b7fb01e3118157c04f93d4f133
Martin Edström <meedstrom91@gmail.com>
2024-02-09 12:41:13 +0100

    Readme

--- massmapper.el
+++ massmapper.el
@@ -19,7 +19,7 @@
 
 ;; Author:  <meedstrom91@gmail.com>
 ;; Created: 2018-08-03
-;; Version: 0.1.4
+;; Version: 0.1.5-pre
 ;; Keywords: convenience
 ;; Homepage: https://github.com/meedstrom/massmapper
 ;; Package-Requires: ((emacs "24.4") (dash "2.19.1") (compat "29.1.4.4"))
